e5fa67b3e6d2cb9ec3fdcb34b229c9fcfbeeb326
d-i-duke <234983679+d-i-duke@users.noreply.github.com>
2025-10-01 22:28:06 +0100

    update to new coords; add read_xy_dataset tests

--- tests/test_core.py
+++ tests/test_core.py
@@ -14,19 +14,20 @@ def xy_dataset() -> pl.DataFrame:
     return pl.DataFrame(
         {
             "id": [1, 2, 3, 4, 5],
-            "x": [500000, 510000, 520000, 530000, 540000],
-            "y": [200000, 210000, 190000, 180000, 220000],
+            "x": [300000, 400000, 500000, 530000, 400000],
+            "y": [200000, 300000, 400000, 180000, 150000],
         }
     )
 
 
 @pytest.fixture(scope="module")
 def latlon_dataset() -> pl.DataFrame:
+    # verified co-ords using qgis to transform those in the xy_dataset
     return pl.DataFrame(
         {
             "id": [1, 2, 3, 4, 5],
-            "lon": [0.0, 0.25, 0.5, -0.25, -0.5],
-            "lat": [52.0, 51.5, 52.5, 53.0, 53.5],
+            "lon": [-3.448079, -2.001428, -0.494362, -0.128329, -2.001375],
+            "lat": [51.689821, 52.597808, 53.487243, 51.503992, 51.249166],
         }
     )
 
@@ -59,15 +60,39 @@ class TestImportDataset:
         df = core._import_dataset(test_files_path / "xy.ndjson")
         assert_frame_equal(df, xy_dataset)
 
-    def test_file_doesnt_exist_returns_error(self, test_files_path):
+    def test_file_doesnt_exist_raises_error(self, test_files_path):
         with pytest.raises(FileNotFoundError, match="Dataset not found: *"):
             core._import_dataset(test_files_path / "xy.unknown")
 
-    def test_unknown_extension_returns_error(self, test_files_path):
+    def test_unknown_extension_raises_error(self, test_files_path):
         with pytest.raises(
             ValueError, match="Dataset file type is not valid, given *"
         ):
             core._import_dataset(test_files_path / "xy.other")
+
+
+class TestReadXYDataset:
+    def test_xy_dataset_missing_x_raises_error(self, xy_dataset):
+        with pytest.raises(
+            ValueError, match="'x' or 'y' columns are not included in the dataset"
+        ):
+            core._read_xy_dataset(xy_dataset, "x_value", "y", 27700)
+
+    def test_xy_dataset_missing_y_raises_error(self, xy_dataset):
+        with pytest.raises(
+            ValueError, match="'x' or 'y' columns are not included in the dataset"
+        ):
+            core._read_xy_dataset(xy_dataset, "x", "y_value", 27700)
+
+    def test_xy_dataset_missing_x_and_y_raises_error(self, xy_dataset):
+        with pytest.raises(
+            ValueError, match="'x' or 'y' columns are not included in the dataset"
+        ):
+            core._read_xy_dataset(xy_dataset, "x_value", "y_value", 27700)
+
+    def test_xy_dataset_returned_as_wgs84(self, xy_dataset, latlon_dataset):
+        df = core._read_xy_dataset(xy_dataset, "x", "y", 27700)
+        assert_frame_equal(df, latlon_dataset)
 
 
 # if __name__ == "__main__":
