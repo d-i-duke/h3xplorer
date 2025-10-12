48d1437023eab028ace8ae7c2a16193658ceb390
d-i-duke <234983679+d-i-duke@users.noreply.github.com>
2025-10-12 20:40:25 +0100

    docstring updates and pass max_threshold through

--- src/h3xplorer/plotting.py
+++ src/h3xplorer/plotting.py
@@ -72,21 +72,34 @@ def normalise_values_diverging(
 
 
 def create_polygon_layer(
-    gdf: gpd.GeoDataFrame, value_col: str, **polygon_formatting
+    gdf: gpd.GeoDataFrame,
+    value_col: str,
+    max_threshold: float | int | None = None,
+    cmap: list[str] | None = None,
+    **polygon_formatting,
 ) -> PolygonLayer:
     """Creates a polygon layer for the given data.
 
+    Colours are plotted using a diverging colour scheme, where by default colours below zero
+    are plotted in blue, and colours above zero are plotted in brown. The default colour scheme
+    was chosen for accessibility.
+
     Args:
         gdf: Spatial dataset containing only polygons.
-        value_col: value of the column in the
+        value_col: value of the column to use for colours.
+        max_threshold: A value to set max/min at in the new normalised set of values. This can
+            make values in the return NDArray be below 0 / above 1.
+            If a max threshold is negative it will be made positive using `abs()`.
+        cmap: the colour map to use. by default this will use the colorcet cmap 'CET_CBL1'.
         **polygon_formatting: Dictionary of PolygonLayer formatting options to their values.
 
     Returns:
-        lonboard map with the given polygon data plotted.
+        lonboard-format polygon layer with the given formatting applied.
     """
-    palette = colorcet_to_palette(cc.CET_CBD1)
+    cmap = cc.CET_CBL1 if cmap is None else cmap
+    palette = colorcet_to_palette(cmap)
     data_values = gdf.loc[:, value_col]
-    norm_values = normalise_values_diverging(data_values)
+    norm_values = normalise_values_diverging(data_values, max_threshold)
     fill_colours = apply_continuous_cmap(norm_values, palette, alpha=0.75)
     line_colours = apply_continuous_cmap(norm_values, palette, alpha=0.9)
     default_format = {
